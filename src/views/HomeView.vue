e356f4e4f96075183fa72da58b91a5afbd23cfb2
joe <joe.ulica@gmail.com>
2022-10-26 12:24:03 -0400

    add Axios, create basic index page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,39 @@
+<script>
+import axios from "axios"
+
+export default {
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      books: []
+    };
+  },
+  created: function () {
+    this.getBooks()
+  },
+  methods: {
+    getBooks: function () {
+      console.log("getting books")
+      axios.get("http://localhost:3000/books.json").then(response => {
+        console.log(response.data)
+        this.books = response.data
+      })
+    }
+  }
+};
+</script>
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <div v-for="book in books">
+      Title: {{ book.title }} <br />
+      Author: {{ book.author.name }} <br />
+      Genres: <p v-for="category in book.categories"> {{ category.name }} </p>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style>
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+</style>
